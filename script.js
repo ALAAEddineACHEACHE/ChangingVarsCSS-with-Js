f801d60ddeea19d62cd826e2f1839b4f6fe25021
ALAE <57995821+ALAAEddineACHEACHE@users.noreply.github.com>
2021-08-09 15:11:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let spacing = document.getElementById("spacing");
+let color = document.getElementById("color");
+let image =document.getElementById("image");
+let blur = document.getElementById("blur");
+spacing.addEventListener("change",function(event){
+    event.preventDefault();
+    image.style.padding=`${spacing.value}px`;
+   
+  
+
+
+});
+color.addEventListener("change",function(){
+    image.style.backgroundColor=color.value;
+})
+blur.addEventListener("blur",function(){
+    image.style.filter=`blur(${blur.value}px)`;
+   
+
+});
+
