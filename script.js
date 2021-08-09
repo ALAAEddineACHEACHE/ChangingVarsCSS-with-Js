bc158c6676697cac99080d083f221d2721796ce0
ALAE <57995821+ALAAEddineACHEACHE@users.noreply.github.com>
2021-08-09 15:12:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,11 @@ let blur = document.getElementById("blur");
 spacing.addEventListener("change",function(event){
     event.preventDefault();
     image.style.padding=`${spacing.value}px`;
-   
-  
-
-
 });
 color.addEventListener("change",function(){
     image.style.backgroundColor=color.value;
 })
 blur.addEventListener("blur",function(){
     image.style.filter=`blur(${blur.value}px)`;
-   
-
 });
 
